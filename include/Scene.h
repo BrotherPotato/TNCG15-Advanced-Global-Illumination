5e21393aaeca9bf7004229e8c9e2067bb9ef5c69
Max Wiklundh <wiklundhmax@gmail.com>
2024-10-03 11:22:57 +0200

    da

---
+++ include/Scene.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// array of objects
+
+// each objects has triangles and a material
+
+#include "Triangle.h"
+#include "vector"
+
+class Scene {
+public:
+
+private:
+	std::vector<Triangle> a;
+};
