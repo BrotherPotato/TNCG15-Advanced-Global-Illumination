453f52c3ce3c192480b89d9211bc6adbf3297522
Max Wiklundh <wiklundhmax@gmail.com>
2024-10-10 11:03:56 +0200

    some materials

--- include/Material.h
+++ include/Material.h
@@ -5,7 +5,12 @@
 class Material {
 public:
 
+
+
 private:
 	ColourRGB _ObjectColour;
+	// använda normal för att bestämma n1 och n2 vid _Transparent
+	enum _MaterialType { _LambertianReflector, _MirrorReflection, _Transparent };
+
 
 };
