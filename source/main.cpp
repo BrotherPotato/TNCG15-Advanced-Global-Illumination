37e31257da828ad5574df73eb76b21be71ce437e
Max Wiklundh <wiklundhmax@gmail.com>
2024-10-28 14:12:57 +0100

    Fixade den riktiga scenen som ska användas sen, checka med whiteboard,  kommenterad just nu

--- source/main.cpp
+++ source/main.cpp
@@ -37,6 +37,67 @@ int main() {
 	glm::vec3 ptr{ glm::vec3(10, -10, -1) };
 	scene.createLightSource(pbl, ptr);
 
+
+	//// Real scene
+	//glm::vec3 T0(13, 0, 5);
+	//glm::vec3 T1(13, 0, -5);
+	//glm::vec3 T2(10, -6, 5);
+	//glm::vec3 T3(10, -6, -5);
+	//glm::vec3 T4(10, 6, 5);
+	//glm::vec3 T5(10, 6, -5);
+
+	//glm::vec3 T6(-3, 0, 5);
+	//glm::vec3 T7(-3, 0, -5);
+	//glm::vec3 T8(0, -6, 5);
+	//glm::vec3 T9(0, -6, -5);
+	//glm::vec3 T10(0, 6, 5);
+	//glm::vec3 T11(0, 6, -5);
+
+	////Roof
+	//Material RoofBlue{ Material::_LambertianReflector ,ColourRGB(0,0,1) };
+	//scene.createTriangle(T6, T8, T10, RoofBlue); // green in whiteboard
+	//scene.createTriangle(T8, T2, T10, RoofBlue); // blue in whiteboard
+	//scene.createTriangle(T2, T4, T10, RoofBlue); // orange in whiteboard
+	//scene.createTriangle(T2, T0, T4, RoofBlue); // red in whiteboard
+
+	////Floor
+	//Material FloorGrey{ Material::_LambertianReflector ,ColourRGB(0.3,0.3,0.3) };
+	//scene.createTriangle(T7, T9, T11, FloorGrey); // green in whiteboard
+	//scene.createTriangle(T9, T3, T11, FloorGrey); // blue in whiteboard
+	//scene.createTriangle(T3, T5, T11, FloorGrey); // orange in whiteboard
+	//scene.createTriangle(T3, T1, T5, FloorGrey); // red in whiteboard
+
+	//// Wall behind camera
+	//Material WallBehindBlack{ Material::_LambertianReflector ,ColourRGB(0,0,0) };
+	//scene.createTriangle(T10, T7, T11, WallBehindBlack); // green in whiteboard
+	//scene.createTriangle(T10, T6, T7, WallBehindBlack); // blue in whiteboard
+	//scene.createTriangle(T7, T6, T8, WallBehindBlack); // orange in whiteboard
+	//scene.createTriangle(T9, T7, T8, WallBehindBlack); // red in whiteboard
+
+	//// Wall left
+	//Material WallLeftPurple{ Material::_LambertianReflector ,ColourRGB(0.5,0,0.5) };
+	//scene.createTriangle(T10, T11, T5, WallLeftPurple); // green in whiteboard
+	//scene.createTriangle(T5, T4, T10, WallLeftPurple); // magenta in whiteboard
+
+	//// Wall right
+	//Material WallRightCyan{ Material::_LambertianReflector ,ColourRGB(0,0.5,0.5) };
+	//scene.createTriangle(T3, T9, T8, WallRightCyan); // green in whiteboard
+	//scene.createTriangle(T8, T2, T3, WallRightCyan); // magenta in whiteboard
+
+	//// Wall infront camera
+	//Material WallFrontRed{ Material::_LambertianReflector ,ColourRGB(1,0,0) };
+	//scene.createTriangle(T4, T5, T1, WallFrontRed); // green in whiteboard
+	//scene.createTriangle(T1, T0, T4, WallFrontRed); // blue in whiteboard
+	//scene.createTriangle(T2, T0, T1, WallFrontRed); // orange in whiteboard
+	//scene.createTriangle(T1, T3, T2, WallFrontRed); // red in whiteboard
+
+	//// Only lightsource
+	//glm::vec3 L0(1, 1, 1);
+	//glm::vec3 L1(1, 1, 1);
+	//scene.createLightSource(L0, L1);
+
+
+
 	// skjut rays
 	camera.emitRays();
 
